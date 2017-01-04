b2bfb61973945935113b521e1f80afd48f8c9cc0
Hensel <douglas.hensel@gmail.com>
2017-01-04 15:52:14 -0500

    init start of node backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const http   = require('http'),
+	  fs     = require('fs'),
+	  server = http.createServer(function( req, res ){
+	  		displayForm(res);
+	  });
+
+function displayForm( res ){
+	fs.readFile('index.html', function( err, data){
+		res.writeHead(200, 
+			'Content-Type' : 'text/html',
+			'Content-Length' : data.length
+		);
+		res.write(data);
+		res.end();
+	});
+}
+
+server.listen(8080);
+console.log("Sever listening on 8080");
