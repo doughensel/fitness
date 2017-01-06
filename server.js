6f9f8bb2753a29cbfb5be533353e4657007cc7df
Douglas Hensel <douglashensel@Doctor.local>
2017-01-05 20:50:53 -0500

    added comments

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const http       = require('http'),
 
 function displayLogin( res ){
 
+	// check is users.json exists -- if not, create it
 	if( !fs.existsSync('json/users.json') ){
 		let users = { user: [] };
 		var json = JSON.stringify(users);
 		fs.writeFile('json/users.json', json, 'utf8' );
 	}
-
+	// load the login page & display it
 	fs.readFile('index.html', function( err, data){
         res.writeHead(200, {
             'Content-Type'   : 'text/html',
@@ -30,20 +31,24 @@ function displayLogin( res ){
 		res.end();
 	});
 
-}
+} // END function displayLogin( res )
 
-function processForms( req, res){
+
+function processForms( req, res ){
 
 	let form  = new formidable.IncomingForm(),
 		users = [],
 		temp  = [];
 
+	// get the submitted form data and save them to a temporary array
 	form.on('field', function( field, value ){
 		temp[field] = value;
 	});
 
+	// when the form has finished processing, jump into the json checking...
 	form.on('end', retrieveUsers);
 
+	// lookup and capture the dsata from user.json
 	function retrieveUsers(){
 		fs.readFile('json/users.json', 'utf8', function( err, data ){
 			if( !err ){
@@ -53,42 +58,59 @@ function processForms( req, res){
 		});
 	}
 
+	// check if the user exists for registration or login
 	function checkUsers(){
 		
+		// checking for registration...
+		// the field name 'regUser' will be passed in the registration
 		if( temp['regUser'] ){
 
+			// see if the username already exists in our users.json
 			let userAccount = findUser( temp['regUser'] );
 
+			/// if the name does not exist, process and add...
 			if( Object.keys(userAccount).length == 0 ){
 
+				// TODO: create a counter in user.json for the ID
+				// create the data, add it to our temp users object, stringify it, and write the file
 				users.user.push({ id: users.user.length, username: temp['regUser'], password: temp['regPass'] });
 				var json = JSON.stringify( users );
 				fs.writeFile('json/users.json', json, 'utf8', dataProcessed );	
 
 			}else{
+				// the username already exists. handle error(s) here
 				dataProcessed({ err : 'Username already exists' });
 			}
 
-		}
+		}// END if( temp['regUser'] )
 
+		// checking for login ...
+		// the field name 'username' will be passed during the log in
 		if( temp['username'] ){
 
+			// see if the username exists
 			let userAccount = findUser( temp['username'] );
 
+			// if the username can be found, process
 			if( Object.keys(userAccount).length > 0 ){
+				// check is the passwords match
 				if( userAccount.password === temp['pass'] ){
 					dataProcessed({ success : 'SUCCESS!' });
 				}else{
+					// bad password error(s) handled here
 					dataProcessed({ err : 'Incorrect Password' });
 				}
 			}else{
+				// incorrect username error(rs) handled here
 				dataProcessed({ err : 'No user with that name' });
 			}
 
-		}
+		}// END if( temp['username'] )
 
-	}
+	}// END function checkUsers()
 
+	// Helper function that runs through the temp users object to find a match
+	// and return the record (if a match is found)
 	function findUser( name ){
 		let userAccount = {}
 		users.user.forEach(function(u, index){
@@ -97,8 +119,10 @@ function processForms( req, res){
 			}
 		});
 		return userAccount;
-	}
+	}// END function findUser( name )
 
+	// End function, after everything is processed...
+	// TODO: A lot ;)
 	function dataProcessed( msg ){
 		fs.readFile('index.html', function( err, data){
 	        res.writeHead(200, {
@@ -114,12 +138,12 @@ function processForms( req, res){
 			res.write(data);
 			res.end();
 		});
-	}
+	} // END function dataProcessed( msg )
 	
 
 	form.parse(req);
 
-}
+} // END function processForms( req, res )
 
 server.listen(port);
 console.log(`Sever listening on ${port}`);
